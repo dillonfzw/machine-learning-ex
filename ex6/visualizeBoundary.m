b9e6474ea3bd47c66d30b41b7b8ce0fd0238990e
Zhiwen Fu <dillonfzw@gmail.com>
2016-11-09 07:43:42 +0800

    ex6: apply an errata about coutour() which visualizeBoundrary

--- ex6/visualizeBoundary.m
+++ ex6/visualizeBoundary.m
@@ -18,7 +18,31 @@ end
 
 % Plot the SVM boundary
 hold on
-contour(X1, X2, vals, [0 0], 'linecolor', 'b');
+
+% Trouble with the contour plot (visualizeBoundary.m)
+%
+% Octave 3.8.x and higher
+%
+% If you have Octave 3.8.x, the ex6 script will not plot decision boundary, and
+% prints 'Unknown hggroup property Color' with stack trace.
+%
+% One fix is to modify line 21 in visualizeBoundary.m with this code:
+% >> 01: contour(X1, X2, vals, [1 1], 'linecolor', 'blue');
+% >> 02:
+% (Note: I tried this and although the error went away, I still don't see any contour line drawn; sokolov 3/22/2015)
+%
+% I had the same problem with the line not displaying until i changed the [0 0] to [1 1] - tmcarthur 7/1/2016
+%
+% OR
+%
+% If you change line 21 to following, it will show two lines and will work with >= 3.8.x .
+% >> 01: contour(X1, X2, vals);
+% >> 02:
+% For more information see
+%
+% http://lists.gnu.org/archive/html/octave-bug-tracker/2014-01/msg00226.html
+contour(X1, X2, vals, [1 1], 'linecolor', 'blue');
+
 hold off;
 
 end
